dab2b07c4292a1da5df5d03326a3cb47f1520084
madhursun <madhursun@gmail.com>
2026-02-21 16:56:05 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirement_lst: List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines= file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print(f"Error: {file_path} not found.")
+
+    return requirement_lst
+
+setup(
+    name='NetworkSecurity',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    author='Madhur Sunhare',
+    author_email='madhursun@gmail.com'
+)
+
