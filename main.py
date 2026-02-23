a821d8f24a2ae0f560c84f66548355dce9ccbe2c
madhursun <madhursun@gmail.com>
2026-02-23 19:53:18 +0530

    data valid complete

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-from networksecurity.components.data_ingestion import DataIngestion 
+from networksecurity.components.data_ingestion import DataIngestion
 import sys
-
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.entity.config_entity import DataIngestionConfig 
+from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
 if __name__ == "__main__":
     try:
-        trainingPipelineConfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingPipelineConfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("data ingestion start")
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
-        print(dataingestionartifact)
-       
+        training_pipeline_config = TrainingPipelineConfig()
+
+        # Data Ingestion
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+
+        logging.info("Data ingestion start")
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion end")
+        print(data_ingestion_artifact)
+
+        # Data Validation
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+
+        data_validation = DataValidation(
+            data_validation_config=data_validation_config,
+            data_ingestion_artifact=data_ingestion_artifact
+        )
+
+        logging.info("Data validation start")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation end")
+        print(data_validation_artifact)
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,31 @@
+import yaml
+import os
+import sys
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
+import numpy as np
+# import dill
+import pandas as pd
+import networksecurity.logging.logger as logging
+from networksecurity.exception.exception import NetworkSecurityException
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+  try:
+    with open(file_path, "rb") as yaml_file:
+      return yaml.safe_load(yaml_file)
+  except Exception as e:
+    raise NetworkSecurityException(e, sys)
+  
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace and os.path.exists(file_path):
+            os.remove(file_path)
+
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, "w") as yaml_file:
+            yaml.dump(content, yaml_file)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
